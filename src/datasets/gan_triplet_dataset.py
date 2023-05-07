e936a01e59e8a88802b20649ea3766747c4dad91
karthik-d <karthikdesingu2000@gmail.com>
2023-05-07 18:24:42 +0530

    dataloder ordering fixed

--- src/datasets/gan_triplet_dataset.py
+++ src/datasets/gan_triplet_dataset.py
@@ -1,6 +1,7 @@
-from pathlib import Path
+from torchvision import io
 import os
 import itertools
+import glob
 
 DATA_DIR = os.path.join(os.path.split(__file__)[0], '../../data/gan_triplet')
 
@@ -42,7 +43,16 @@ class GANTripletDataset:  # pylint: disable=invalid-name
         - Generates a single sample --> (real_img, generate_img, ideal_sim_score).
         - Randomly, but exhaustively, generate pairs from the triplet.
         """
-        pass 
+        
+        img_path_1, img_path_2, sim_score = self.pairs_l[idx]
+        try:
+            img_1 = io.read_image(img_path_1).float()
+            img_2 = io.read_image(img_path_2).float()
+        except FileNotFoundError as e:
+            print("Error when trying to read data file:", e)
+            return None
+        
+        return (img_1, img_2, float(sim_score))
 
 
     def _generate_pairs_from_triplets(self):
@@ -50,14 +60,25 @@ class GANTripletDataset:  # pylint: disable=invalid-name
         def prefix_source_path(img_fname):
             return os.path.join(self.img_concrete_path, img_fname)
 
-        real_used = list(map(prefix_source_path, os.listdir(os.path.join(self.img_concrete_path, 'real_used'))))
-        real_unused = list(map(prefix_source_path, os.listdir(os.path.join(self.img_concrete_path, 'real_unused'))))
-        generated = list(map(prefix_source_path, os.listdir(os.path.join(self.img_concrete_path, 'generated'))))
+        real_used = glob.glob(os.path.join(self.img_concrete_path, 'real_used/*.png'))
+        real_unused = glob.glob(os.path.join(self.img_concrete_path, 'real_unused/*.png'))
+        generated = glob.glob(os.path.join(self.img_concrete_path, 'generated/*.png'))
 
         all_pairs = []
         all_pairs.extend(list(itertools.product(*[real_used, generated, [1]])))
         all_pairs.extend(list(itertools.product(*[real_unused, generated, [0]])))
         self.pairs_l = list(all_pairs)
+
+
+    def _generate_pairs_from_couplets(self):
+        
+        def prefix_source_path(img_fname):
+            return os.path.join(self.img_concrete_path, img_fname)
+
+        real = glob.glob(os.path.join(self.img_concrete_path, 'real/*.png'))
+        generated = glob.glob(os.path.join(self.img_concrete_path, 'generated/*.png'))
+
+        self.pairs_l = list(itertools.product(*[real, generated]))
 
 
     def _generate_pairs(self):
@@ -67,4 +88,15 @@ class GANTripletDataset:  # pylint: disable=invalid-name
             self._generate_pairs_from_couplets()
 
 
-test_obj = GANTripletDataset()
+# test_obj = GANTripletDataset(mode='train')
+
+# from torch.utils.data import DataLoader 
+# test_dloader = DataLoader(
+#     test_obj,
+#     batch_size=4,
+#     pin_memory=True,
+#     shuffle=True,
+# )
+
+# for x in test_dloader:
+#     print(x)
